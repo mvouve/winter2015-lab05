9dc85f6d49205bc706794e41f7615aad03fd3a6f
Marc Vouve <mvouve@gmail.com>
2015-02-22 14:34:55 -0800

    added add method

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -17,6 +17,7 @@ class Admin extends Application
     function __construct() 
     {
         parent::__construct();
+        $this->load->helper('formfields');
     }
 
     function index() 
@@ -25,6 +26,25 @@ class Admin extends Application
         $this->data['quotes'] = $this->quotes->all();
         $this->data['pagebody'] = 'admin_list';
         $this->render();
+    }
+    
+    function add()
+    {
+        $quote = $this->quotes->create();
+        $this->present($quote);
+    }
+    
+    function present($quote) 
+    {
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
+        $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
+        $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
+        $this->data['pagebody'] = 'quote_edit';
+        $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quotation data", 'btn-success'); 
+        $this->render();
+        
+        
     }
 
 }
---
+++ application/views/quote_edit.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <form action="/admin/confirm">
+        {fid}
+        {fwho}
+        {fmug}
+        {fwhat}
+        {fsubmit}
+    </form>
+</div>
+
