bc1ec3ce6b66efcf4a4c07e500be6c07473ce221
Marc Vouve <mvouve@gmail.com>
2015-02-22 15:17:40 -0800

    added error checking

--- application/config/constants.php
+++ application/config/constants.php
@@ -47,6 +47,8 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+// custom defines
+define('BR', '<br/>');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -36,15 +36,47 @@ class Admin extends Application
     
     function present($quote) 
     {
-        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $message = '';
+        if (count($this->errors) > 0) {
+            foreach ($this->errors as $booboo)
+            $message .= $booboo . BR;
+        } 
+        $this->data['message'] = $message;
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id, "Unique quote identifier, system-assigned",10,10,true); 
         $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
         $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
         $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
         $this->data['pagebody'] = 'quote_edit';
         $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quotation data", 'btn-success'); 
-        $this->render();
-        
-        
+        $this->render();  
     }
+    
+    function confirm() 
+    {
+        $record = $this->quotes->create();
+        $record->id = $this->input->post('id');
+        $record->who = $this->input->post('who');
+        $record->mug = $this->input->post('mug');
+        // Extract submitted fields
+        $record->what = $this->input->post('what');
+        
+        if (empty($record->who))
+            $this->errors[] = 'You must specify an author.';
+        if (strlen($record->what) < 20)
+            $this->errors[] = 'A quotation must be at least 20 characters long.';
+        
+        if (count($this->errors) > 0) {
+            $this->present($record);
+            return; 
+        }
+        
+        if (empty($record->id)) 
+            $this->quotes->add($record);
+        else 
+            $this->quotes->update($record);
+        
+
+        redirect('/admin');
+    } 
 
 }
